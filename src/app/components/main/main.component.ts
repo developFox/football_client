aed34ae63ae0d32e4aa86d8fbc4da4e9f558c252
Linaz Rizvanov <rizvan.linaz@gmail.com>
2020-07-20 13:48:11 +0300

    Главная страница

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,12 +1,34 @@
 import {Component, OnInit} from '@angular/core';
 import {MainService} from './main.service';
+import {RatingsService} from '../ratings/ratings.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
 })
 export class MainComponent {
-  constructor(private mainService: MainService) {
+  ratings: InterFacePlayers[] = [];
+  filter = {
+    limit: 5,
+    sort: 1990,
+    filter: ''
+  };
 
+  constructor(private mainService: MainService,
+              private ratingsService: RatingsService) {
+    this.getPlayers();
+  }
+
+  getPlayers() {
+    this.ratingsService.getRatings({
+      limit: this.filter.limit,
+      sort: this.filter.sort,
+      filter: this.filter.filter,
+    }).then((data: InterFacePlayers[]) => {
+        this.ratings = data;
+      },
+      (error) => {
+        console.log('Ошибка при получении рейтинга: ', error);
+      });
   }
 }
