ffae886f8b64cb205abfde632f51913230997e85
Linaz Rizvanov <rizvan.linaz@gmail.com>
2020-08-31 12:04:39 +0300

    Новый select

---
+++ script/update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo 'Запуск обновления';
+rm -rf dist;
+ng build --configuration=production;
+#cp .htaccess dist/football_client/
+scp -r dist/client/* evolov_footbal@evolov.beget.tech:/home/e/evolov/footcapital.ru/public_html
+#ssh rdwork_linaz_front@rdwork.beget.tech '
+#rm -rf public_html/*;
+#mv client-arenda/* public_html/;
+#rm -rf client-arenda;'
+echo 'Конец обновления';
