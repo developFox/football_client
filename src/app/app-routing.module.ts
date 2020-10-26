73d0520615796afc9c39a9d46a8019447a5d300b
Linaz Rizvanov <rizvan.linaz@gmail.com>
2020-10-26 13:14:16 +0300

    [checked]="params.checked.val" (click)="params.checked.val=!params.checked.val"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -97,7 +97,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule],
   providers: []
 })
